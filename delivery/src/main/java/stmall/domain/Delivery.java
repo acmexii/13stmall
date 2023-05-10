4359ae68563b2ef4459d9f3fb0471725cb5059db
acmexii <apexacme@gmail.com>
2023-05-10 04:00:21 +0000

    code updated

--- delivery/src/main/java/stmall/domain/Delivery.java
+++ delivery/src/main/java/stmall/domain/Delivery.java
@@ -16,31 +16,24 @@ public class Delivery {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private Long orderId;
-
     private String productName;
-
     private Long productId;
-
     private Long userId;
-
     private Integer qty;
-
     private String status;
-
     private String courier;
 
     @PostPersist
     public void onPostPersist() {
-        DeliveryStarted deliveryStarted = new DeliveryStarted(this);
-        deliveryStarted.publishAfterCommit();
+        // DeliveryStarted deliveryStarted = new DeliveryStarted(this);
+        // deliveryStarted.publishAfterCommit();
     }
 
     @PostUpdate
     public void onPostUpdate() {
-        DeliveryCancelled deliveryCancelled = new DeliveryCancelled(this);
-        deliveryCancelled.publishAfterCommit();
+        // DeliveryCancelled deliveryCancelled = new DeliveryCancelled(this);
+        // deliveryCancelled.publishAfterCommit();
     }
 
     public static DeliveryRepository repository() {
@@ -50,63 +43,51 @@ public class Delivery {
         return deliveryRepository;
     }
 
-    public void deliveryComplete(
-        DeliveryCompleteCommand deliveryCompleteCommand
-    ) {
+    public void deliveryComplete(DeliveryCompleteCommand deliveryCompleteCommand) {
+        this.setCourier(deliveryCompleteCommand.getCourier());
+        this.setStatus("DeliveryCompleted");
+
         DeliveryCompleted deliveryCompleted = new DeliveryCompleted(this);
         deliveryCompleted.publishAfterCommit();
     }
 
     public void deliveryReturn(DeliveryReturnCommand deliveryReturnCommand) {
+        this.setCourier(deliveryReturnCommand.getCourier());
+        this.setStatus("DeliveryReturned");
+
         DeliveryReturned deliveryReturned = new DeliveryReturned(this);
         deliveryReturned.publishAfterCommit();
     }
 
     public static void startDelivery(OrderPlaced orderPlaced) {
-        /** Example 1:  new item 
+        /** Example 1:  new item         */
         Delivery delivery = new Delivery();
+        delivery.setOrderId(orderPlaced.getId());
+        delivery.setProductId(orderPlaced.getProductId());
+        delivery.setProductName(orderPlaced.getProductName());
+        delivery.setQty(orderPlaced.getQty());
+        delivery.setUserId(orderPlaced.getUserId());
+        delivery.setStatus("DeliveryStarted");
         repository().save(delivery);
 
         DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
         deliveryStarted.publishAfterCommit();
-        */
 
-        /** Example 2:  finding and process
-        
-        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
-            
-            delivery // do something
-            repository().save(delivery);
-
-            DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
-            deliveryStarted.publishAfterCommit();
-
-         });
-        */
 
     }
 
     public static void cancelDelivery(OrderCancelled orderCancelled) {
-        /** Example 1:  new item 
-        Delivery delivery = new Delivery();
-        repository().save(delivery);
-
-        DeliveryCancelled deliveryCancelled = new DeliveryCancelled(delivery);
-        deliveryCancelled.publishAfterCommit();
-        */
-
-        /** Example 2:  finding and process
+        /** Example 2:  finding and process     */
         
-        repository().findById(orderCancelled.get???()).ifPresent(delivery->{
-            
-            delivery // do something
+        repository().findByOrderId(orderCancelled.getId()).ifPresent(delivery->{
+            delivery.setStatus("DeliveryCancelled");
             repository().save(delivery);
 
             DeliveryCancelled deliveryCancelled = new DeliveryCancelled(delivery);
             deliveryCancelled.publishAfterCommit();
 
          });
-        */
+   
 
     }
 }
